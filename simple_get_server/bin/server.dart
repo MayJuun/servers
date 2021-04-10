ca1fe3d8e1cd744c3a47601132837a87ff54e55b
Dokotela <jgfaulkenberry@gmail.com>
2021-04-10 16:53:04 -0400

    Simplified get_server

--- simple_get_server/bin/server.dart
+++ simple_get_server/bin/server.dart
@@ -1,20 +1,13 @@
 import 'package:get_server/get_server.dart';
 
-void main() {
-  runApp(
-    GetServer(
-      home: FolderWidget('app/web'),
-      getPages: [
-        GetPage(name: '/welcome', page: () => Welcome()),
-      ],
-    ),
-  );
-}
-
-class Welcome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final path = 'app/web/index.html';
-    return Html(path: path);
-  }
-}
+void main() => runApp(
+      GetServer(
+        home: FolderWidget('app/web'),
+        getPages: [
+          GetPage(
+            name: '/welcome',
+            page: () => Html(path: 'app/web/index.html'),
+          ),
+        ],
+      ),
+    );
